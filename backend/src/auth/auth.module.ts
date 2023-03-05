32cd554c908bb5ae1b837a331a9b813e69cbab76
biku1998 <kumarsourabh228@gmail.com>
2023-03-05 12:01:50 +0530

    (save) checkpoint, adding integration test stuff

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './services';
 import { AuthController } from './controllers';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './strategies/jwt.strategy';
--- backend/src/auth/services/auth.service.spec.ts
+++ backend/src/auth/services/auth.service.spec.ts
@@ -1,21 +1,24 @@
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
+import { EventsModule } from 'src/events/events.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  it('should pass', () => {
-    expect(1).toBe(1);
+  let service: AuthService;
+  let jwtService: JwtService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AuthService],
+      imports: [UsersModule, JwtModule, PrismaModule],
+    }).compile();
+    service = module.get<AuthService>(AuthService);
   });
-  // let service: AuthService;
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     providers: [AuthService],
-  //   }).compile();
-
-  //   service = module.get<AuthService>(AuthService);
-  // });
-
-  // it('should be defined', () => {
-  //   expect(service).toBeDefined();
-  // });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
 });
