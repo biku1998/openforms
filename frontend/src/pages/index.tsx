c885651a314e59a9d29a55d61c0c0b20b9607635
biku1998 <kumarsourabh228@gmail.com>
2023-04-04 11:22:05 +0530

    (feat/frontend) add storybook support

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,15 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home () {
-  return <div className="p-6">
-    <h1 className="text-2xl font-extrabold">Components :</h1>
-    <hr className="mb-4"/>
+export default function Home() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-extrabold">Components :</h1>
+      <hr className="mb-4" />
 
-    <div className="flex flex-col w-fit gap-3">
-    <Button>Hello world</Button>
-    <Button size={'lg'} >Hello world</Button>
+      <div className="flex flex-col w-fit gap-3">
+        <Button>Hello world</Button>
+        <Button size={"lg"}>Hello world</Button>
+      </div>
     </div>
-  </div>
+  );
 }
